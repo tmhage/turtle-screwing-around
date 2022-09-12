f3a5c26d54f004a3c963fcfa243641f04cfcb9c0
Timmo Hage <attmhage@gmail.com>
2022-09-12 20:46:12 +0200

    Make turtle run a square

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+turtle.shape("turtle")
+turtle.color("red")
+turtle.forward(180)
+turtle.right(90)
+turtle.forward(180)
+turtle.right(90)
+turtle.forward(180)
+turtle.right(90)
+turtle.forward(180)
+
+
+screen = Screen()
+screen.exitonclick()
