33adedf2034cda53dc9d9eed92a3288ffa02e298
Timmo Hage <attmhage@gmail.com>
2022-09-12 21:38:56 +0200

    Change colour of turtle every completion

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from turtle import Turtle, Screen
+import random
 
 jimmy = Turtle()
+
+
+def change_color(turtle):
+    r = random.random()
+    b = random.random()
+    g = random.random()
+    turtle.color(r, b, g)
+
 
 for corners in range(3, 10):
     turn = 360 / corners
@@ -8,6 +17,8 @@ for corners in range(3, 10):
     for x in range(corners):
         jimmy.forward(100)
         jimmy.right(turn)
+
+    change_color(jimmy)
 
 screen = Screen()
 screen.exitonclick()
