362983e17bc1274de90d7e50e5bd0709660ae1a9
Timmo Hage <attmhage@gmail.com>
2022-09-14 13:28:58 +0200

    Implement random_walk method

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def change_color(turtle):
     g = random.random()
     turtle.color(r, b, g)
 
+
 def draw_shape(sides, turtle):
     change_color(turtle)
     turn = 360 / sides
@@ -19,8 +20,17 @@ def draw_shape(sides, turtle):
         turtle.right(turn)
 
 
-for sides in range(3, 11):
-    draw_shape(sides, jimmy)
+def random_walk(turtle):
+    change_color(turtle)
+    turtle.forward(10)
+    turns = [0, 90, 180, 270]
+    turtle.setheading(random.choice(turns))
+
+
+# for x in range(3, 11):
+#     draw_shape(x, jimmy)
+while True:
+    random_walk(jimmy)
 
 
 screen = Screen()
